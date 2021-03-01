9c5b0499fc267bcc7f24ad458424a62fbbd247c0
udaymondal <udaysk.mondal@gmail.com>
2021-03-01 09:23:40 +0600

    Ulta-Nation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(()=> {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => {
+      setCountries(data);
+      const names = data.map(country => country.name);
+    })
+    .catch(error => console.log(error))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Countries Loaded: {countries.length}</h3>
+      <ul>
+        {
+          countries.map(country => <li>{country.name}</li>)
+        }
+      </ul>
     </div>
   );
 }
